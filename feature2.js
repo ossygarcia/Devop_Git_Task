b17dee73f203b50758f05d95de402e70c807ecad
oseghale Ehichioya Israel <mrbrucekessy@gmail.com>
2022-12-12 12:26:05 +0100

    Create feature2.js (#3)

---
+++ feature2.js
@@ -0,0 +1,10 @@
+const product =[
+  {id:1, name="Ose", address="Minna",phone="090755484833"},
+   {id:2, name="Jame", address="Abuja",phone="090755484833"},
+  {id:3, name="Paul", address="Kano",phone="090755484833"},
+  {id:4, name="Grace", address="Kwara",phone="090755484833"},
+  {id:5, name="Tom", address="Osun",phone="090755484833"},
+  {id:6, name="Jerry", address="Lagos",phone="090755484833"},
+  {id:7, name="Blessing", address="Ibadan",phone="090755484833"},
+  {id:8, name="Peace", address="Anambra",phone="090755484833"},
+]
