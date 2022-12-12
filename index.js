0162afdb0bffd904ab7b98a7f50bf6fcbc5ae137
oseghale Ehichioya Israel <mrbrucekessy@gmail.com>
2022-12-12 11:27:37 +0100

    Update and rename index.html to index.js
    modified commit in master

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("Express")
 const app = express()
 
-const PORT = 5000
+const PORT = 3000
+app.get("/", (re,res)=>{
+  res.status(200)
+})
 
 app.listening(PORT. console.(server running))
